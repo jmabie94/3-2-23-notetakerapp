920f3ddce4c228f2dadcf706142de596a7139d92
Jack Mabie <johnmabie94@gmail.com>
2023-03-01 17:24:37 -0600

    adds starter code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+
+const PORT = process.env.PORT||3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+  
+app.get('/api/notes', (req, res) => res.json(db));
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at ${PORT}`);
+  });
